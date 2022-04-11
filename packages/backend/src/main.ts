321eaa259a5d8295c6154b9b51f867469011513e
Esteban <exteban16@gmail.com>
2022-04-10 20:30:54 -0500

    Add scripts for heroku

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
+  const port = process.env.PORT || 3001;
+  await app.listen(port, '0.0.0.0', () => {
+    console.log('Listening at http://localhost:' + port);
+  });
 }
 bootstrap();
